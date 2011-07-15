8e27e30a5c368eb11c961bd8b147240df6a781d4
kernel.writer@gmail.com <kernel.writer@gmail.com@2a44d478-8128-94f7-2576-ae5751d19af7>
2011-07-15 20:24:36 +0000

    First commit of the CGAL-DLL ---------------------------- There are two projects: i) CGAL-DLL-Core, a dynamic library project which uses the CGAL libary for Convex hull algorithm and ii)DllTest console application which uses the dll created.

---
+++ src/CGAL-DLL/include/common.h
@@ -0,0 +1,14 @@
+/*
+ * The following ifdef block is the standard way of creating macros which make exporting 
+ * from a DLL simpler. All files within this DLL are compiled with the CGALDLL_EXPORTS
+ * symbol defined on the command line. this symbol should not be defined on any project
+ * that uses this DLL. This way any other project whose source files include this file see 
+ * CGALDLL_API functions as being imported from a DLL, whereas this DLL sees symbols
+ * defined with this macro as being exported.
+ */
+
+#ifdef CGALDLLCORE_EXPORTS
+#define CGALDLL_API __declspec(dllexport)
+#else
+#define CGALDLL_API __declspec(dllimport)
+#endif
