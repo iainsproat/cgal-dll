b1f7ce6a637e897acaa87c420f34ac4b21e4c457
kernel.writer@gmail.com <kernel.writer@gmail.com@2a44d478-8128-94f7-2576-ae5751d19af7>
2011-07-16 06:52:23 +0000

    - a commit with compiling error in test project. In this attempt, I'm trying to create dll with same signature as the cgal.

--- src/CGAL-DLL/CGAL-DLL-Core/convex_hull_2.cpp
+++ src/CGAL-DLL/CGAL-DLL-Core/convex_hull_2.cpp
@@ -1,19 +1,9 @@
-#include "convex_hull_2.h"
+#include "../include/convex_hull_2.h"
 #include <CGAL/convex_hull_2.h>
 
-convex_hull_2::convex_hull_2(void)
+template <class ForwardIterator, class OutputIterator>
+CGALDLL_API OutputIterator  CGAL_DLL::convex_hull_2 (ForwardIterator first, ForwardIterator last, 
+              OutputIterator  result)
 {
-}
-
-
-Points convex_hull_2::calcConvexHull(const Points &points)
-{
-	Points result;
-	CGAL::convex_hull_2( points.begin(), points.end(), std::back_inserter(result) );
-	return result;
-}
-
-
-convex_hull_2::~convex_hull_2(void)
-{
+	return CGAL::convex_hull_2<ForwardIterator, OutputIterator> ( first, last, result);
 }
