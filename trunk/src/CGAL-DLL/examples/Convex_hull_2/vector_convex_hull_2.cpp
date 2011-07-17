3adf7fb31a3aae1eebdc229984be92c39c766387
iainsproat@gmail.com <iainsproat@gmail.com@2a44d478-8128-94f7-2576-ae5751d19af7>
2011-07-17 10:24:33 +0000

    split the examples back into two files

--- trunk/src/CGAL-DLL/examples/Convex_hull_2/vector_convex_hull_2.cpp
+++ trunk/src/CGAL-DLL/examples/Convex_hull_2/vector_convex_hull_2.cpp
@@ -1,7 +1,7 @@
 #include <convex_hull_2.h>
 #include <CGAL/convex_hull_2.h>
 
-void vector_convex_hull_2(){
+int main(){
   Points points, result;
 
   points.push_back(Point_2(0,0));
@@ -13,21 +13,6 @@ void vector_convex_hull_2(){
   PointsBackIterator resultIterator = std::back_inserter(result);
 
   CGAL_DLL::convex_hull_2( points.begin(), points.end(), resultIterator);
-  std::cout << result.size() << " points on the convex hull from a vector" << std::endl;
-}
-
-void array_convex_hull_2(){
-	Point_2 points[5] = { Point_2(0,0), Point_2(10,0), Point_2(10,10), Point_2(6,5), Point_2(4,1) };
-    Point_2 result[5];
-
-    Point_2 *ptr = CGAL::convex_hull_2( points, points+5, result );
-    std::cout <<  ptr - result << " points on the convex hull from an array" << std::endl;
-
-}
-
-int main()
-{
-  vector_convex_hull_2();
-  array_convex_hull_2();
+  std::cout << result.size() << " points on the convex hull" << std::endl;
   return 0;
 }
